c5e25fae9782cb23f0b7dc38ebc4ecd3504574e8
tahayahmed <tahayahmed@yahoo.com>
2025-04-10 15:43:29 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_LuVpzugUA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '27etkoftcvovu9b18b37qj66ud', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_YHOcnRHEM', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6p0b4657qc90n872es2gfsvu1r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
